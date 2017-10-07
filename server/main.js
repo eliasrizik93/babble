9b86f9864995ed9d576f08f83d08546ee6e6b5a4
elias rizik <eliasrizik93@gmail.com>
2017-10-07 19:19:17 +0300

    babble

---
+++ server/main.js
@@ -0,0 +1,223 @@
+﻿/*jslint this, node: true */
+"use strict";
+
+var http = require("http");
+var url = require("url");
+
+var messages = require("./messages-util");
+var users = require("./users-util");
+
+var port = 9000;
+var server = http.createServer(function (req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-USER-ID");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+
+    var requestUrl = url.parse(req.url, true);
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(204);
+        res.end("HTTP 204 No Content");
+        return;
+    }
+
+    if (req.headers["content-type"] !== "application/json") {
+        res.writeHead(400);
+        res.end("HTTP 400 Bad Request");
+        return;
+    }
+
+    //POST /messages
+    //GET /messages?counter=XX
+    if (requestUrl.pathname === "/messages") {
+        if (req.method === "GET") {
+            if (!requestUrl.query || !requestUrl.query.counter || isNaN(requestUrl.query.counter) || Object.keys(requestUrl.query).length > 1) {
+                res.writeHead(400);
+                res.end("HTTP 400 Bad Request");
+                return;
+            }
+
+            if (!req.headers["x-user-id"] || !users.isLoggedIn(req.headers["x-user-id"])) {
+                res.writeHead(400);
+                res.end("HTTP 400 Bad Request");
+                return;
+            }
+
+            var counter = requestUrl.query.counter;
+            var newMessages = messages.getMessages(counter);
+            if (newMessages && newMessages.length > 0) {
+                var payload = {messages: newMessages, count: messages.getMessageCount()};
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(payload));
+                return;
+            }
+
+            users.addToMessageClients(req.headers["x-user-id"], res, counter);
+            return;
+
+        } else if (req.method === "POST") {
+
+            var msgPostPayload = "";
+            req.on("data", function (data) {
+                msgPostPayload += data;
+            });
+            req.on("end", function () {
+                var message = {};
+                try {
+                    message = JSON.parse(msgPostPayload);
+                } catch (e) {
+                    res.writeHead(400);
+                    res.end("HTTP 400 Bad Request");
+                    return;
+                }
+
+                if (!Object.keys(message).includes("message")) {
+                    res.writeHead(400);
+                    res.end("HTTP 400 Bad Request");
+                    return;
+                }
+
+                if (!req.headers["x-user-id"] || !users.isLoggedIn(req.headers["x-user-id"])) {
+                    res.writeHead(400);
+                    res.end("HTTP 400 Bad Request");
+                    return;
+                }
+
+                message.userId = req.headers["x-user-id"];
+                var messageId = messages.addMessage(message);
+                users.updateAllMessageClients();
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({id: messageId}));
+                return;
+
+            });
+            return;
+
+        } else {
+            res.writeHead(405);
+            res.end("HTTP 405 Method Not Allowed");
+            return;
+        }
+    }
+
+    //DELETE /messages/:id
+    if (requestUrl.pathname.substring(0, 10) === "/messages/") {
+        if (req.method !== "DELETE") {
+            res.writeHead(405);
+            res.end("HTTP 405 Method Not Allowed");
+            return;
+        }
+
+        var messageId = requestUrl.pathname.substring(10);
+        if (isNaN(messageId)) {
+            res.writeHead(404);
+            res.end("HTTP 404 Not found");
+            return;
+        }
+
+        if (!req.headers["x-user-id"] || !users.isLoggedIn(req.headers["x-user-id"])) {
+            res.writeHead(400);
+            res.end("HTTP 400 Bad Request");
+            return;
+        }
+
+        messages.deleteMessage(messageId);
+        users.updateAllMessageClients();
+        res.writeHead(200);
+        res.end("HTTP 200 Success");
+        return;
+    }
+
+    //GET /stats
+    if (requestUrl.pathname === "/stats") {
+        if (req.method !== "GET") {
+            res.writeHead(405);
+            res.end("HTTP 405 Method Not Allowed");
+            return;
+        }
+
+        if (!req.headers["x-user-id"] || !users.isLoggedIn(req.headers["x-user-id"])) {
+            res.writeHead(400);
+            res.end("HTTP 400 Bad Request");
+            return;
+        }
+
+        //change to true if you want to use long polling for stats call
+        var longPolling = false;
+
+        if (longPolling) {
+            users.addToStatsClients(req.headers["x-user-id"], res);
+            return;
+        } else {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({userCount: users.getConnectedUsers(), messageCount: messages.getMessageCount()}));
+            return;
+        }
+    }
+
+    //POST /register
+    if (requestUrl.pathname === "/register") {
+        if (req.method !== "POST") {
+            res.writeHead(405);
+            res.end("HTTP 405 Method Not Allowed");
+            return;
+        }
+
+        //User already logged in ?
+        if (req.headers["x-user-id"] && users.isLoggedIn(req.headers["x-user-id"])) {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({id: req.headers["x-user-id"]}));
+            return;
+        }
+
+        var regPayload = "";
+        req.on("data", function (data) {
+            regPayload += data;
+        });
+        req.on("end", function () {
+            var message = {};
+            try {
+                message = JSON.parse(regPayload);
+            } catch (e) {
+                res.writeHead(400);
+                res.end("HTTP 400 Bad Request");
+                return;
+            }
+
+            var userId = users.login(message.name, message.email);
+            users.updateAllStatsClients();
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({id: userId}));
+            return;
+        });
+        return;
+    }
+
+    //POST /unregister
+    if (requestUrl.pathname === "/unregister") {
+        if (req.method !== "POST") {
+            res.writeHead(405);
+            res.end("HTTP 405 Method Not Allowed");
+            return;
+        }
+
+        if (!req.headers["x-user-id"] || !users.isLoggedIn(req.headers["x-user-id"])) {
+            res.writeHead(400);
+            res.end("HTTP 400 Bad Request");
+            return;
+        }
+
+        users.logout(req.headers["x-user-id"]);
+        users.updateAllStatsClients();
+        res.writeHead(200);
+        res.end("HTTP 200 Success");
+        return;
+    }
+
+    res.writeHead(404);
+    res.end("HTTP 404 Not found");
+    return;
+}).listen(port);
+
+module.exports = server;
---
+++ server/messages-util.js
@@ -0,0 +1,49 @@
+﻿/*jslint this, node: true */
+"use strict";
+
+var users = require("./users-util");
+
+var Messages = {
+
+    //{name: String, email:String, message:String,timestamp:Number(ms)}
+
+    messageArray: [],
+
+    //messages.addMessage(message:Object) : Number(id)
+    addMessage: function (message) {
+        message.id = this.guid();
+        this.messageArray.push(message);
+        return message.id;
+    },
+
+    //messages.getMessages(counter:Number) : Array(messages)
+    getMessages: function (counter) {
+        if (counter >= this.messageArray.length) {
+            return [];
+        }
+        var messages = this.messageArray.slice(counter);
+        messages.forEach(function (msg) {msg.avatar = users.getUserById(msg.userId).avatar;});
+        return messages;
+    },
+
+    // messages.deleteMessage(id:String) : void
+    deleteMessage: function (id) {
+        this.messageArray = this.messageArray.filter(function (msg) {
+            return msg.id === id;
+        });
+    },
+
+    //messages.getMessageCount() : Number(count)
+    getMessageCount: function () {
+        return this.messageArray.length;
+    },
+
+    guid: function () {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c === "x" ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+};
+
+module.exports = Messages;
